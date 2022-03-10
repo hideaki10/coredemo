526cd03f0f68094abafab69ab1c8c06b4345b380
hideaki10 <senshikou1985@gmail.com>
2022-03-10 22:43:54 +0900

    add controller

--- router/user_controller.go
+++ router/user_controller.go
@@ -3,6 +3,7 @@ package router
 import "github.com/hideaki10/coredemo/framework"
 
 func UserLoginController(c *framework.Context) error {
-	c.Json(200, "login success")
+	//c.Json(200, "login success")
+	c.SetOkStatus().Json("login success")
 	return nil
 }
